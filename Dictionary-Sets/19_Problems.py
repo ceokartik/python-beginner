323260d9041a676314b7dc52ee0c4242f0c45849
Kartik Bathla <kartik@pvaindia.com>
2021-10-30 18:24:39 +0530

    Probelms of Dictionary

---
+++ Dictionary-Sets/19_Problems.py
@@ -0,0 +1,45 @@
+#PROB 1
+
+# dict1={
+#     "kela": "banana",
+#     "gora": "fair",
+#     "kala": "dark"
+# }
+# print("options are",dict1.keys())
+# Hindi=input("Enter Hindi Word \n")
+# print("The meaning is",dict1.get(Hindi))
+
+#PROB 2
+
+# a=input("Enter Number 1 = ")
+# b=input("Enter Number 2 = ")
+# c=input("Enter Number 3 = ")
+# d=input("Enter Number 4 = ")
+# e=input("Enter Number 5 = ")
+# f=input("Enter Number 6 = ")
+# g=input("Enter Number 7 = ")
+# h=input("Enter Number 8 = ")
+
+# set1 = {a,b,c,d,e,f,g,h}
+# print(set1)
+
+#PROB 3
+
+# set1={20,20.0,"20"}
+# print(len(set1))
+
+#PROB5
+
+lang={}
+
+a = input("Enter your fav lang a = ")
+b = input("Enter your fav lang b = ")
+c = input("Enter your fav lang c = ")
+d = input("Enter your fav lang d = ")
+
+lang['a']=a
+lang['b']=b
+lang['c']=c
+lang['d']=d
+
+print(lang)
